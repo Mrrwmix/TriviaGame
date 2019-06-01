cc5b46e0856ed6d5bd2f9f4e18d2c381ed36fb3b
Matthew Winemiller <seidoc@gmail.com>
2019-05-31 19:21:45 -0700

    Initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+// Global variables
+
+var correct = 0;
+var incorrect = 0;
+var questionNumber = 0;
+
+
+// On click button to start trivia
+
+
+
+// On click button to restart game (without reloading the page)
+
+
+// Checking answer or timeout function
+
+
