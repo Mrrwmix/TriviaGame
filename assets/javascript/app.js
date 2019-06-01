31bcf15b10559b56821bb658215bda104924e511
Matthew Winemiller <seidoc@gmail.com>
2019-05-31 20:05:36 -0700

    Styling and pseudocoding

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,6 +12,6 @@ var questionNumber = 0;
 // On click button to restart game (without reloading the page)
 
 
-// Checking answer or timeout function
+// Checking answer or timeout function. Add classes bg-danger and bg-success classes to wrong/right answers. Clear them before loading the next question.
 
 
