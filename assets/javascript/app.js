67db6d4dcc022935b2adfb5ebe592c7e048cfb54
Matthew Winemiller <seidoc@gmail.com>
2019-06-02 22:09:38 -0700

    functioning game, needs results screen, unanswered variable, and reset function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,38 +2,49 @@
 
 var correct = 0;
 var incorrect = 0;
+var unanswered = 0;
 var questionNumber = 0;
 var secondsLeft = 30;
+var timer;
+var loadingTimer;
+var gifSeconds = 4;
 
 
 // On click button to start trivia
 $(document).ready(function () {
-    $("#start").on("click", function(){
+    $("#start").on("click", function () {
         $("#instructions").empty();
-        questionNumber++;
-        var timer = setInterval(function () {
-            $("#instructions").html("<p><h2 class='text-danger'>" + "Time remaining: " + secondsLeft + "</h2></p>");
-            secondsLeft--;
-            $("#instructions").html("<p><h2 class='text-danger'>" + "Time remaining: " + secondsLeft + "</h2></p>");
-            if (secondsLeft == 0){
-                clearInterval(timer);
-                incorrect++;
-            }
-        }, 1000);
-        $("#question1").css("display", "block");
-    
+        changeQuestion();
     })
 
 
-    // On click button to restart game (without reloading the page)
+    // Changing questions function
+
+    function changeQuestion() {
+        questionNumber++;
+        $("#question" + (questionNumber)).css("display", "block");
+        $("#instructions").html("<p><h2 class='text-danger'>" + "Time remaining: " + secondsLeft + "</h2></p>");
+        timer = setInterval(function () {
+            $("#instructions").html("<p><h2 class='text-danger'>" + "Time remaining: " + secondsLeft + "</h2></p>");
+            secondsLeft--;
+            $("#instructions").html("<p><h2 class='text-danger'>" + "Time remaining: " + secondsLeft + "</h2></p>");
+            if (secondsLeft == 0) {
+                clearInterval(timer);
+                secondsLeft = 30;
+                unanswered++;
+            }
+        }, 1000);
+    }
 
 
     // Checking answer or timeout function. Add classes bg-danger and bg-success classes to wrong/right answers. Clear them before loading the next question.
 
     // $("button").attr("value","wrong").toggleClass("btn-secondary btn-danger") switch secondary and danger to reset this
-    $(".btn-secondary").on("click", function(){
+    $(".btn-secondary").on("click", function () {
         var gotThisCorrect;
-        if ($(this).attr("value") === "correct"){
+        clearInterval(timer);
+        secondsLeft = 30;
+        if ($(this).attr("value") === "correct") {
             console.log("You clicked the right answer!");
             correct++;
             gotThisCorrect = true;
@@ -42,14 +53,49 @@ $(document).ready(function () {
             incorrect++;
             gotThisCorrect = false;
         }
-        $("button").each(function(index){
-            if ($(this).val() === "correct"){
+        $(".btn-secondary").attr("disabled", true); // set this to false in the next question function
+        $("button").each(function (index) {
+            if ($(this).val() === "correct") {
                 $(this).toggleClass("btn-secondary btn-success");
             }
             else {
                 $(this).toggleClass("btn-secondary btn-danger")
             }
-        })
+        });
+
+        setTimeout(function () {
+            $("button").each(function (index) {
+                if ($(this).val() === "correct") {
+                    $(this).toggleClass("btn-success btn-secondary");
+                }
+                else {
+                    $(this).toggleClass("btn-danger btn-secondary")
+                }
+
+            })
+            $(".btn-secondary").attr("disabled", false);
+            $("#question" + (questionNumber)).css("display", "none");
+            if (gotThisCorrect){
+                $("#correctLoading").css("display", "block");
+            }
+            else {
+                $("#wrongLoading").css("display", "block");
+            }
+
+        }, 1000)
+        $(".loadingTimer").text("Next question in " + gifSeconds + "!");
+        loadingTimer = setInterval(function () {
+            gifSeconds--;
+            $(".loadingTimer").text("Next question in " + gifSeconds + "!");
+        }, 1000);
+
+        setTimeout(function() {
+            clearInterval(loadingTimer);
+            gifSeconds = 4;
+            $("#correctLoading").css("display", "none");
+            $("#wrongLoading").css("display", "none");
+            changeQuestion();
+        }, 4000);
 
     })
 
